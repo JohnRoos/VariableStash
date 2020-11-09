9498d22ff779d5cf3aafd70853c79d658a34acbc
John Roos <john.roos@advania.com>
2020-11-09 13:08:14 +0100

    Added name and index parameters

--- Public/Get-VarStash.ps1
+++ Public/Get-VarStash.ps1
@@ -1,7 +1,27 @@
 function Get-VarStash {
-    param()
+    [CmdletBinding(DefaultParameterSetName = '__AllParameterSets')]
+    param(
+        [parameter(ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
-    $stashes = Get-ChildItem -Path "$env:APPDATA\VariableStash\VariableStash_*.xml"  | Sort-Object -Property lastwritetime -Descending
+        [parameter(ParameterSetName = 'Index')]
+        [ValidateRange(0, [int]::MaxValue)]
+        [int]$Index
+    )
+    
+    switch ($PSCmdlet.ParameterSetName) {
+        'Name' { 
+            $stashes = @(Get-ChildItem -Path "$env:APPDATA\VariableStash\VariableStash_$Name.xml"  | Sort-Object -Property lastwritetime -Descending)
+        }
+        'Index' { 
+            $stashes = @(Get-ChildItem -Path "$env:APPDATA\VariableStash\VariableStash_*.xml"  | Sort-Object -Property lastwritetime -Descending | Select-Object -First 1 -Skip $Index)  
+        }
+        Default {
+            $stashes = @(Get-ChildItem -Path "$env:APPDATA\VariableStash\VariableStash_*.xml"  | Sort-Object -Property lastwritetime -Descending)
+        }
+    }
+    
     foreach ($stash in $stashes) {
         $props = @{
             Name = $stash.Name.Replace('VariableStash_','').Replace('.xml','')
