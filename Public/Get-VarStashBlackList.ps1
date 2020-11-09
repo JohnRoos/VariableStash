aa2fbc3763dfd1c3fe961d5a5a51fdd2e1cb95ad
John Roos <john.roos@advania.com>
2020-11-09 13:08:42 +0100

    Added manual variable names in blacklist

--- Public/Get-VarStashBlackList.ps1
+++ Public/Get-VarStashBlackList.ps1
@@ -4,49 +4,55 @@ function Get-VarStashBlackList {
 
     # Static list: https://ss64.com/ps/syntax-automatic-variables.html
     $static = '$',
-    '?',
-    '^',
-    '_',
-    'Allnodes',
-    'Args',
-    'ConsoleFileName',
-    'Error',
-    'Event',
-    'EventArgs',
-    'EventSubscriber',
-    'ExecutionContext',
-    'False',
-    'ForEach',
-    'Home',
-    'Host',
-    'Input',
-    'LastExitCode',
-    'Matches',
-    'MyInvocation',
-    'NestedPromptLevel',
-    'NULL',
-    'OFS',
-    'PID',
-    'Profile',
-    'PSBoundParameters',
-    'PsCmdlet',
-    'PSCommandPath',
-    'PsCulture',
-    'PSDebugContext',
-    'PsHome',
-    'PSitem',
-    'PSScriptRoot',
-    'PSSenderInfo',
-    'PsUICulture',
-    'PsVersionTable',
-    'Pwd',
-    'Sender',
-    'ShellID',
-    'SourceArgs',
-    'SourceEventArgs',
-    'StackTrace',
-    'This',
-    'True'
+        '?',
+        '^',
+        '_',
+        'Allnodes',
+        'Args',
+        'ConsoleFileName',
+        'Error',
+        'Event',
+        'EventArgs',
+        'EventSubscriber',
+        'ExecutionContext',
+        'False',
+        'ForEach',
+        'Home',
+        'Host',
+        'Input',
+        'LastExitCode',
+        'Matches',
+        'MyInvocation',
+        'NestedPromptLevel',
+        'NULL',
+        'OFS',
+        'PID',
+        'Profile',
+        'PSBoundParameters',
+        'PsCmdlet',
+        'PSCommandPath',
+        'PsCulture',
+        'PSDebugContext',
+        'PsHome',
+        'PSitem',
+        'PSScriptRoot',
+        'PSSenderInfo',
+        'PsUICulture',
+        'PsVersionTable',
+        'Pwd',
+        'Sender',
+        'ShellID',
+        'SourceArgs',
+        'SourceEventArgs',
+        'StackTrace',
+        'This',
+        'True'
+
+    # Manually added
+    $ManuallyAdded = 'MaximumHistoryCount',
+        'PSDefaultParameterValues',
+        'psEditor'
+
     
     # Variables from inital session state
     $RunspaceSessionState = (Get-Runspace).InitialSessionState.Variables.Name
@@ -54,6 +60,9 @@ function Get-VarStashBlackList {
     # Mash them together
     $BlackList = [System.Collections.Generic.HashSet[String]]::new()
     foreach ($item in $static) {
+        $null = $BlackList.Add($item)
+    }
+    foreach ($item in $ManuallyAdded) {
         $null = $BlackList.Add($item)
     }
     foreach ($item in $RunspaceSessionState) {
