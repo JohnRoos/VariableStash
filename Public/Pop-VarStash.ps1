bbcf7617b476dd969ea0c4c58cec6c6d1f031079
John Roos <john.roos@advania.com>
2020-11-09 13:09:28 +0100

    added parameters to Pop-VarStash

--- Public/Pop-VarStash.ps1
+++ Public/Pop-VarStash.ps1
@@ -1,23 +1,52 @@
 function Pop-VarStash {
-    [CmdletBinding()]
-    param ()
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param(
+        [parameter(ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
+        [parameter(ParameterSetName = 'Index')]
+        [ValidateRange(0, [int]::MaxValue)]
+        [int]$Index,
+
+        [parameter(ParameterSetName = 'Name')]
+        [parameter(ParameterSetName = 'Index')]
+        [parameter(ParameterSetName = 'Default')]
+        [switch]$Keep
+    )
+
+    switch ($PSCmdlet.ParameterSetName) {
+        'Name' { 
+            $Stash = Get-VarStash -Name $Name
+        }
+        'Index' { 
+            $Stash = Get-VarStash -Index $Index
+        }
+        Default {
+            $Stash = Get-VarStash -Index 0
+        }
+    }
+
+    $StashPath = "$env:APPDATA\VariableStash\VariableStash_$($Stash.Name).xml"
     # Check if we have a file to import
-    if (Test-Path -Path "$env:APPDATA\VariableStash\VariableStash.xml" -PathType 'Leaf') {  
+    if (Test-Path -Path $StashPath -PathType 'Leaf') {  
         # Import the variables
-        $ImportedVariables = Import-Clixml -Path "$env:APPDATA\VariableStash\VariableStash.xml"
+        $ImportedVariables = Import-Clixml -Path $StashPath
         # Set each variable in parent scope
         foreach ($var in $ImportedVariables) {
             if (Get-Variable -Name $var.Name -Scope 1 -ErrorAction SilentlyContinue) {
-                Write-Verbose "$($var.Name) exist"
+                Write-Verbose "Variable '$($var.Name)' will be overwritten."
             }
             else {
-                Write-Verbose "$($var.Name) missing"
+                Write-Verbose "Variable '$($var.Name)' will be created."
             }
             Set-Variable -Name $var.Name -Scope 1 -Value $var.Value
         }
+        if (-not $Keep.IsPresent) {
+            Remove-VarStash -Name $Stash.Name
+        }
     }
     else {
-        Write-Error "Stash not found"
+        Write-Error "Stash not found."
     }
 }
