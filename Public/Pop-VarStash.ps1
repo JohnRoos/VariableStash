0942a8b37a472aa02c27a5305ce31dc2c63593ee
John Roos <john.roos@advania.com>
2020-11-10 21:17:40 +0100

    Improved parameter validation and error handling

--- Public/Pop-VarStash.ps1
+++ Public/Pop-VarStash.ps1
@@ -1,52 +1,86 @@
 function Pop-VarStash {
-    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    [CmdletBinding(DefaultParameterSetName = 'Keep')]
     param(
-        [parameter(ParameterSetName = 'Name')]
+        [parameter(ParameterSetName = 'Name',
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$Name,
 
-        [parameter(ParameterSetName = 'Index')]
+        [parameter(ParameterSetName = 'Index',
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
         [ValidateRange(0, [int]::MaxValue)]
         [int]$Index,
 
         [parameter(ParameterSetName = 'Name')]
         [parameter(ParameterSetName = 'Index')]
-        [parameter(ParameterSetName = 'Default')]
+        [parameter(ParameterSetName = 'Keep')]
         [switch]$Keep
     )
 
-    switch ($PSCmdlet.ParameterSetName) {
-        'Name' { 
-            $Stash = Get-VarStash -Name $Name
-        }
-        'Index' { 
-            $Stash = Get-VarStash -Index $Index
-        }
-        Default {
-            $Stash = Get-VarStash -Index 0
-        }
-    }
+    Process {
 
-    $StashPath = "$env:APPDATA\VariableStash\VariableStash_$($Stash.Name).xml"
-    # Check if we have a file to import
-    if (Test-Path -Path $StashPath -PathType 'Leaf') {  
-        # Import the variables
-        $ImportedVariables = Import-Clixml -Path $StashPath
-        # Set each variable in parent scope
-        foreach ($var in $ImportedVariables) {
-            if (Get-Variable -Name $var.Name -Scope 1 -ErrorAction SilentlyContinue) {
-                Write-Verbose "Variable '$($var.Name)' will be overwritten."
+        switch ($PSCmdlet.ParameterSetName) {
+            'Name' { 
+                $Stash = Get-VarStash -Name $Name
             }
-            else {
-                Write-Verbose "Variable '$($var.Name)' will be created."
+            'Index' { 
+                $Stash = Get-VarStash -Index $Index
             }
-            Set-Variable -Name $var.Name -Scope 1 -Value $var.Value
+            Default {
+                $Stash = Get-VarStash -Index 0
+            }
         }
-        if (-not $Keep.IsPresent) {
-            Remove-VarStash -Name $Stash.Name
+
+        $StashPath = "$env:APPDATA\VariableStash\VariableStash_$($Stash.Name).xml"
+        # Check if we have a file to import
+        if (Test-Path -Path $StashPath -PathType 'Leaf') {  
+            # Import the variables
+            $ImportedVariables = Import-Clixml -Path $StashPath
+
+            # Set each variable in parent scope
+            foreach ($var in $ImportedVariables) {
+                try {
+                    $null = Get-Variable -Name $var.Name -Scope 2 -ErrorAction Stop
+                    Write-Verbose "Scope 2: Variable '$($var.Name)' will be overwritten."
+                }
+                catch [System.Management.Automation.PSArgumentOutOfRangeException] {
+                    # If we get here, the scope number 2 exceeds the number of active scopes
+                    # This can happen if the function is not executing within a module.
+                    # (This probably only happens during development)
+                    # In that case we use Scope 1 instead
+                    try {
+                        $null = Get-Variable -Name $var.Name -Scope 1 -ErrorAction Stop
+                        Write-Verbose "Scope 1: Variable '$($var.Name)' will be overwritten."
+                        Set-Variable -Name $var.Name -Scope 1 -Value $var.Value
+                    }
+                    catch [System.Management.Automation.PSArgumentOutOfRangeException] {
+                        Throw "Unable to get current variables. Have tried both scope 2 and 1. Error: $_"
+                    }
+                    catch [System.Management.Automation.ItemNotFoundException] {
+                        # Scope works. Variable not found in current session
+                        Write-Verbose "Scope 1: Variable '$($var.Name)' will be created."
+                        Set-Variable -Name $var.Name -Scope 1 -Value $var.Value
+                    }
+                    catch {
+                        Throw "Unable to get current variables in scope 1. Unhandled error. Error: $_"
+                    }
+                }
+                catch {
+                    # Scope works. Variable not found in current session
+                    Write-Verbose "Scope 2: Variable '$($var.Name)' will be created."
+                    Set-Variable -Name $var.Name -Scope 2 -Value $var.Value
+                }
+            }
+
+            if (-not $Keep.IsPresent) {
+                Remove-VarStash -Name $Stash.Name
+            }
         }
-    }
-    else {
-        Write-Error "Stash not found."
+        else {
+            Write-Error -Category ObjectNotFound -Message 'Stash not found.'
+        }
+
     }
 }
