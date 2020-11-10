6ba83fd4e76dfee399d9b97fe4a1a5cdb19d743f
John Roos <john.roos@advania.com>
2020-11-10 21:22:19 +0100

    Improved parameter validation

--- Public/Show-VarStash.ps1
+++ Public/Show-VarStash.ps1
@@ -1,31 +1,39 @@
 function Show-VarStash {
     [CmdletBinding(DefaultParameterSetName = '__AllParameterSets')]
     param(
-        [parameter(ParameterSetName = 'Name')]
+        [parameter(ParameterSetName = 'Name',
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$Name,
 
-        [parameter(ParameterSetName = 'Index')]
+        [parameter(ParameterSetName = 'Index',
+                   ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
         [ValidateRange(0, [int]::MaxValue)]
         [int]$Index
     )
     
-    switch ($PSCmdlet.ParameterSetName) {
-        'Name' { 
-            $Stash = Get-VarStash -Name $Name
-            if ($Stash -is [array]) {
-                Throw 'Found more than one VarStash. Can only show one stash.'
+    Process {
+
+        switch ($PSCmdlet.ParameterSetName) {
+            'Name' { 
+                $Stash = Get-VarStash -Name $Name
+                if ($Stash -is [array]) {
+                    Throw 'Found more than one VarStash. Can only show one stash.'
+                }
+            }
+            'Index' { 
+                $Stash = Get-VarStash -Index $Index
+            }
+            Default {
+                $Stash = Get-VarStash -Index 0
             }
         }
-        'Index' { 
-            $Stash = Get-VarStash -Index $Index
-        }
-        Default {
-            $Stash = Get-VarStash -Index 0
-        }
+
+        $ImportedVariables = Import-Clixml -Path "$env:APPDATA\VariableStash\VariableStash_$($Stash.Name).xml"
+
+        Write-Output -InputObject $ImportedVariables
+        
     }
-
-    $ImportedVariables = Import-Clixml -Path "$env:APPDATA\VariableStash\VariableStash_$($Stash.Name).xml"
-
-    Write-Output -InputObject $ImportedVariables
 }
