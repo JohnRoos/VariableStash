b18f269328099601035490a4fbba25fc8b55f2a8
John Roos <john.roos@advania.com>
2020-11-10 21:17:21 +0100

    Moved to process block

--- Public/Get-VarStashBlackList.ps1
+++ Public/Get-VarStashBlackList.ps1
@@ -2,73 +2,75 @@ function Get-VarStashBlackList {
     [CmdletBinding()]
     param()
 
-    # Static list: https://ss64.com/ps/syntax-automatic-variables.html
-    $static = '$',
-        '?',
-        '^',
-        '_',
-        'Allnodes',
-        'Args',
-        'ConsoleFileName',
-        'Error',
-        'Event',
-        'EventArgs',
-        'EventSubscriber',
-        'ExecutionContext',
-        'False',
-        'ForEach',
-        'Home',
-        'Host',
-        'Input',
-        'LastExitCode',
-        'Matches',
-        'MyInvocation',
-        'NestedPromptLevel',
-        'NULL',
-        'OFS',
-        'PID',
-        'Profile',
-        'PSBoundParameters',
-        'PsCmdlet',
-        'PSCommandPath',
-        'PsCulture',
-        'PSDebugContext',
-        'PsHome',
-        'PSitem',
-        'PSScriptRoot',
-        'PSSenderInfo',
-        'PsUICulture',
-        'PsVersionTable',
-        'Pwd',
-        'Sender',
-        'ShellID',
-        'SourceArgs',
-        'SourceEventArgs',
-        'StackTrace',
-        'This',
-        'True'
+    Process {
+        # Static list: https://ss64.com/ps/syntax-automatic-variables.html
+        $static = '$',
+            '?',
+            '^',
+            '_',
+            'Allnodes',
+            'Args',
+            'ConsoleFileName',
+            'Error',
+            'Event',
+            'EventArgs',
+            'EventSubscriber',
+            'ExecutionContext',
+            'False',
+            'ForEach',
+            'Home',
+            'Host',
+            'Input',
+            'LastExitCode',
+            'Matches',
+            'MyInvocation',
+            'NestedPromptLevel',
+            'NULL',
+            'OFS',
+            'PID',
+            'Profile',
+            'PSBoundParameters',
+            'PsCmdlet',
+            'PSCommandPath',
+            'PsCulture',
+            'PSDebugContext',
+            'PsHome',
+            'PSitem',
+            'PSScriptRoot',
+            'PSSenderInfo',
+            'PsUICulture',
+            'PsVersionTable',
+            'Pwd',
+            'Sender',
+            'ShellID',
+            'SourceArgs',
+            'SourceEventArgs',
+            'StackTrace',
+            'This',
+            'True'
 
-    # Manually added
-    $ManuallyAdded = 'MaximumHistoryCount',
-        'PSDefaultParameterValues',
-        'psEditor'
+        # Manually added
+        $ManuallyAdded = 'MaximumHistoryCount',
+            'PSDefaultParameterValues',
+            'psEditor'
 
-    
-    # Variables from inital session state
-    $RunspaceSessionState = (Get-Runspace).InitialSessionState.Variables.Name
+        
+        # Variables from inital session state
+        $RunspaceSessionState = (Get-Runspace).InitialSessionState.Variables.Name
 
-    # Mash them together
-    $BlackList = [System.Collections.Generic.HashSet[String]]::new()
-    foreach ($item in $static) {
-        $null = $BlackList.Add($item)
+        # Mash them together
+        $BlackList = [System.Collections.Generic.HashSet[String]]::new()
+        foreach ($item in $static) {
+            $null = $BlackList.Add($item)
+        }
+        foreach ($item in $ManuallyAdded) {
+            $null = $BlackList.Add($item)
+        }
+        foreach ($item in $RunspaceSessionState) {
+            $null = $BlackList.Add($item)
+        }
+
+        # Done
+        Write-Output $BlackList
     }
-    foreach ($item in $ManuallyAdded) {
-        $null = $BlackList.Add($item)
-    }
-    foreach ($item in $RunspaceSessionState) {
-        $null = $BlackList.Add($item)
-    }
-
-    # Done
-    Write-Output $BlackList
 }
