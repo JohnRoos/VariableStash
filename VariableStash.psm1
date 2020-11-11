2bf97f7a1eb3fc087f2ceaab1f55292727a44f56
John Roos <john.roos@advania.com>
2020-11-11 08:56:09 +0100

    updated module import script

--- VariableStash.psm1
+++ VariableStash.psm1
@@ -1,9 +1,3 @@
-# import private functions
-foreach ($function in (Get-ChildItem "$PSScriptRoot\Private\*.ps1"))
-{
-	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
-}
-
 # import public functions
 foreach ($function in (Get-ChildItem "$PSScriptRoot\Public\*.ps1"))
 {
